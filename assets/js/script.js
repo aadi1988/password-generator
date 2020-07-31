1cd07029d563a52ca16ce117f0602f08a2701a10
Aaditi <aadi2007.aaditi@gmail.com>
2020-07-31 16:56:11 -0700

    Add random password generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 // Assignment code here
+var random_gen = function(start,end,str1,password){
+  for (var i=start; i<end; i++){
+    var rand_num = Number (Math.random() * str1.length);
+    password[i] = str1.charAt(rand_num);
+    }
+  return password;
+}
+
+
 var generatePassword = function(){
    var password_length = window.prompt("What is the password length: Choose a value between 8 and 128");
    if (password_length < 8 || password_length > 128 || password_length == null || password_length == "" ){
@@ -25,6 +34,63 @@ var generatePassword = function(){
    }
 
   window.alert(arr);
+  if (arr.length == 0) {
+       window.prompt("Choose atleast one character type");
+  }
+       
+
+  var ch_lower = "abcdefghijklmnopqrstuvwxyz";
+  var ch_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var num = "0123456789";
+  var special = "~!@#$%^&*()";
+  var password = [];
+  var start;
+  var end;
+  for (var i=0; i< arr.length; i++){
+       switch(arr[i]){
+           case "lowercase":
+                start = 0;
+                end = Number(Math.floor(password_length/arr.length));
+                str1 = ch_lower;
+                password = random_gen(start,end,str1,password);
+                console.log("lowercase");
+                
+                break;
+           case "uppercase":
+                start = password.length;
+                end = start + Number(Math.floor(password_length/arr.length));
+                str1 = ch_upper;
+                password = random_gen(start,end,str1,password);
+                console.log("uppercase");
+                break;
+           case "numeric":
+                start = password.length;
+                end = start + Number(Math.floor(password_length/arr.length));
+                str1 = num;
+                password = random_gen(start,end,str1,password);
+                console.log("numeric");
+                break;
+           case "special characters":
+                start = password.length;
+                end = start + Number(Math.floor(password_length/arr.length));
+                str1 = special;
+                password = random_gen(start,end,str1,password);
+                console.log("special");
+                break;
+           
+       }   
+  }
+  
+  window.alert(password);
+  if (password.lenth != password_length){
+        
+        start = password.length;
+        end = start + (password_length - start);
+        console.log(end);
+        str1 = ch_lower;
+        password = random_gen(start,end,str1,password);
+  }
+  window.alert(password);
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
