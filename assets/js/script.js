7b54d968b3be5c41b55f135dd1e038502eb6bdcf
Aaditi <aadi2007.aaditi@gmail.com>
2020-08-07 21:23:49 -0700

    add randomization to the password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-// Assignment code here
+var ch_lower = "abcdefghijklmnopqrstuvwxyz";
+var ch_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var special = "~!@#$%^&*()";
+
+var start;
+var end;
+var str1;
+// Random password generator
 var random_gen = function(start,end,str1,password){
   for (var i=start; i<end; i++){
     var rand_num = Number (Math.random() * str1.length);
@@ -7,18 +15,40 @@ var random_gen = function(start,end,str1,password){
   return password;
 }
 
+//Pick a random option to generate the rest of the password if password length is not the same as required password length 
+var password_randomizer = function(password,password_length,arr){
+  if (password.lenth != password_length){
+    
+    var rand_ind = Math.floor(Math.random()*arr.length);
+    str1 = arr[rand_ind];
+    password = random_gen(password.length,password_length,str1,password);
+  }
+  //randomize the password once all options given by the user have been used.
+  for (let i=password.length-1;i>0;i--){
+    
+    let letter = Math.floor(Math.random() * i);
+    var temp;
+    temp = password[i];
+    password[i] = password[letter];
+    password[letter] = temp;
+    }
+    
+    return password;
+}
+
+
+//get options for the character types from the user.
 var getinput = function(input,arr,input_str){
     var prompt_str  = "Choose Yes if " + input_str + " characters should be included, No if not:"
     input = window.prompt(prompt_str);
     if (input.toLowerCase() == "yes"){
-        console.log(arr);
-        console.log(input_str);
+       
         arr.push(input_str);
         
         
     }
     else if(input.toLowerCase() != "yes" && input.toLowerCase() != "no"){
-      console.log(input);
+      
       window.alert("Please enter a valid value");
       getinput(input,arr,input_str);
     }
@@ -26,6 +56,7 @@ var getinput = function(input,arr,input_str){
     return arr;
 }
 
+//calling function to get input from the user to get the character types
 var call_getinput = function(){
   var arr = [];
   var input;
@@ -36,22 +67,20 @@ var call_getinput = function(){
   return arr;
 }
 
-var password_gen = function(arr,len){
-  var ch_lower = "abcdefghijklmnopqrstuvwxyz";
-  var ch_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var num = "0123456789";
-  var special = "~!@#$%^&*()";
-  var password = [];
-  var start;
-  var end;
-  for (var i=0; i< arr.length; i++){
-       switch(arr[i]){
+
+//Create an initial password from the user given options.
+var password_gen = function(arr,len,password){
+  
+  
+
+    for (var j=0; j<arr.length;j++){
+       switch(arr[j]){
            case "lowercase":
-                start = 0;
-                end = Number(Math.floor(len/arr.length));
+                start = password.length;
+                end = start + Number(Math.floor(len/arr.length));
                 str1 = ch_lower;
                 password = random_gen(start,end,str1,password);
-                console.log("lowercase");
+                
                 
                 break;
            case "uppercase":
@@ -59,30 +88,32 @@ var password_gen = function(arr,len){
                 end = start + Number(Math.floor(len/arr.length));
                 str1 = ch_upper;
                 password = random_gen(start,end,str1,password);
-                console.log("uppercase");
+                
                 break;
            case "numeric":
                 start = password.length;
                 end = start + Number(Math.floor(len/arr.length));
                 str1 = num;
                 password = random_gen(start,end,str1,password);
-                console.log("numeric");
+                
                 break;
            case "special characters":
                 start = password.length;
                 end = start + Number(Math.floor(len/arr.length));
                 str1 = special;
                 password = random_gen(start,end,str1,password);
-                console.log("special");
+                
                 break;
-           
-       }   
+        }  
+         
   }
+  
   return password;
 }
 
-
+//Main function to generate thee password which calls all other functions.
 var generatePassword = function(){
+  var password = [];
   var password_length = window.prompt("What is the password length: Choose a value between 8 and 128");
   if (password_length < 8 || password_length > 128 || password_length == null || password_length == "" ){
           window.prompt("Invalid password, Please enter password length again");
@@ -96,20 +127,12 @@ var generatePassword = function(){
        call_getinput();
    }
        
-  var password = password_gen(arr,password_length);
+  password = password_gen(arr,password_length,password);
   
+  password = password_randomizer(password,password_length,arr);
   
-  window.alert(password);
-  var ch_lower = "abcdefghijklmnopqrstuvwxyz";
-  if (password.lenth != password_length){
-        
-        start = password.length;
-        end = start + (password_length - start);
-        console.log(end);
-        str1 = ch_lower;
-        password = random_gen(start,end,str1,password);
-  }
-  window.alert(password);
+  return password.join("");
+  
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
